7d66e3f0e0795109c0041ee3548a9590bd2ed019
arafatrahman41 <arafatrahman41@outlook.com>
2024-07-04 22:38:53 +0600

    save some items

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -16,15 +16,15 @@ useEffect(() => {
     axiosSecure.interceptors.response.use(res => {
         return res;
     }, error => {
-        console.log("error tracked in the interceptor",error.response);
-        // if(error.response.status === 401 || error.response.status === 403){
-        //     console.log("logout the user");
-        //     logOut()
-        //     .then(() =>{
-        //         navigate("/login")
-        //     })
-        //     .catch( error => console.log(error))
-        // }
+        // console.log("error tracked in the interceptor",error.response);
+        if(error.response.status === 401 || error.response.status === 403){
+            // console.log("logout the user");
+            logOut()
+            .then(() =>{
+                navigate("/login")
+            })
+            .catch( error => console.log(error))
+        }
     })
 },[logOut, navigate])
 
